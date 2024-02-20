f48b954ec8eff78db9bb342aca50eaba73230a36
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2024-02-20 09:28:26 +0100

    store shared schema config persistently

--- libsql-server/src/namespace/mod.rs
+++ libsql-server/src/namespace/mod.rs
@@ -457,6 +457,11 @@ impl<M: MakeNamespace> NamespaceStore<M> {
         })
     }
 
+    pub async fn exists(&self, namespace: &NamespaceName) -> bool {
+        let e = self.inner.store.get(namespace).await;
+        e.is_some()
+    }
+
     pub async fn destroy(&self, namespace: NamespaceName) -> crate::Result<()> {
         if self.inner.has_shutdown.load(Ordering::Relaxed) {
             return Err(Error::NamespaceStoreShutdown);
@@ -710,7 +715,6 @@ impl<M: MakeNamespace> NamespaceStore<M> {
         namespace: NamespaceName,
         restore_option: RestoreOption,
         bottomless_db_id: NamespaceBottomlessDbId,
-        shared_schema: Option<SharedSchemaOptions>,
     ) -> crate::Result<()> {
         // With namespaces disabled, the default namespace can be auto-created,
         // otherwise it's an error.
@@ -834,34 +838,6 @@ impl<T: Database> Namespace<T> {
 
     pub fn stats(&self) -> Arc<Stats> {
         self.stats.clone()
-    }
-}
-
-pub enum SharedSchemaOptions {
-    Seed,
-    Link(NamespaceName),
-}
-
-impl SharedSchemaOptions {
-    pub fn new(
-        is_shared_schema: bool,
-        shared_schema_name: Option<String>,
-    ) -> crate::Result<Option<Self>> {
-        if is_shared_schema {
-            if shared_schema_name.is_some() {
-                Err(Error::SharedSchemaError(
-                    "shared-schema database cannot reference to other shared schema database"
-                        .to_string(),
-                ))
-            } else {
-                Ok(Some(Self::Seed))
-            }
-        } else if let Some(name) = shared_schema_name {
-            let ns = NamespaceName::from_string(name)?;
-            Ok(Some(SharedSchemaOptions::Link(ns)))
-        } else {
-            Ok(None)
-        }
     }
 }
 
